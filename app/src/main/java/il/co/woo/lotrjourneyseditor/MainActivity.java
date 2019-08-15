a1f9470dd5c186f672397702203fc3efb0e40d8a
Itamar <zipo13@gmail.com>
2019-08-15 11:00:17 +0300

    First Checkin

---
+++ app/src/main/java/il/co/woo/lotrjourneyseditor/MainActivity.java
@@ -0,0 +1,14 @@
+package il.co.woo.lotrjourneyseditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
